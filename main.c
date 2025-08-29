cdf8b5dd2f1daf1e8bb66caec4773df09fad77c5
zlowrogi2k <wasilewskiolek880@gmail.com>
2025-08-29 20:45:58 +0000

    updated the main.c file with basic code

--- main.c
+++ main.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void km-m-conversion(float a, float b)
+void helpMsg()
 {
+	printf("To convert from kilometers to miles choose: 1\n");
+	printf("To convert from miles to kilometers choose: 2\n\n");
 }
 
-void m-km-conversion(float a, float b)
+void kmMConversion(float a)
 {
+	float kmM = a * 0.621371F;
+}
+
+void mKmConversion(float a)
+{
+	float mKm = a / 0.621371F;
 }
 
 int main()
 {
+	char choice;
+	float num;
+
+	// Welcome message and usage guide
+	printf("Welcome to the conversion program!\n");
+	printf("You can convert kilometers to miles and vice versa. Just choose one of the options:\n");
+	helpMsg();
+	printf("Do you understand the instructions? [y/n] ");
+
+	// Helper handling
+	scanf("%c", &choice);
+	if (choice == 'y')
+	{
+		// handle for yes
+	}
+	else if (choice == 'n')
+	{
+		// handle for no
+	}
+
+	printf("Which option would you like to use? [1 / 2 / help] ");
+
+	// Conversion handling
 	return 0;
 }
 
