f43868f790790639b0f7eb75e8c1e08a90dbb168
zlowrogi2k <wasilewskiolek880@gmail.com>
2025-08-30 10:38:22 +0000

    Added handling for conversion, option 'h' not supported yet

--- main.c
+++ main.c
@@ -7,49 +7,46 @@ void helpMsg()
 	printf("To convert from miles to kilometers choose: 2\n\n");
 }
 
-void kmMConversion(float a)
+double kmMConversion(float a)
 {
-	float kmM = a * 0.621371F;
+	return a * 0.621371;
 }
 
-void mKmConversion(float a)
-{
-	float mKm = a / 0.621371F;
+double mKmConversion(float a)
+{	
+	return a / 0.621371;;
 }
 
 int main()
 {
 	char choice;
-	float num;
+	float distance;
+	float output;
 
 	// Welcome message and usage guide
 	printf("\nWelcome to the conversion program!\n");
 	printf("You can convert kilometers to miles and vice versa. Just choose one of the options:\n");
 	helpMsg();
-	printf("Do you understand the instructions? [y/n] ");
-
-	// Helper handling
-	scanf("%c", &choice);
-	if (choice == 'y')
-	{
-		// handle for yes
-	}
-	else if (choice == 'n')
-	{
-		// handle for no
-	}
 	
-	scanf("%c", &choice);
 	printf("\nWhich option would you like to use? [1 / 2 / h] ");
 
 	// Conversion handling
+	scanf("%c", &choice);
 	if (choice == '1')
 	{
-		// handle 1
+		printf("Converting from kilometers to miles\n");
+		printf("Enter distance in kilometers: ");
+		scanf("%f", &distance);
+		output = kmMConversion(distance);
+		printf("\n%f kilometers converted to miles is %f\n", distance, output);
 	}
 	else if (choice == '2')
 	{
-		// handle 2
+		printf("Converting from miles to kilometers\n");
+		printf("Enter distance in miles: ");
+		scanf("%f", &distance);
+		output = mKmConversion(distance);
+		printf("\n%f kilometers converted to miles is %f\n", distance, output);
 	}
 	else if (choice == 'h')
 	{
